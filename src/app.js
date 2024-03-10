37a5667907a04399d50e194cf7557b75c59b656d
dajana-lab <75635513+dajana-lab@users.noreply.github.com>
2024-03-10 21:58:58 +0100

    Added the link

--- src/app.js
+++ src/app.js
@@ -37,13 +37,14 @@ function updateCity(event) {
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `<div class="city">
         <div>
-          <h2>${cityName}</h2>
+          <h2><em>${cityName}</em></h2>
           <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
         <div class="time">${cityTime.format(
           "h:mm:ss"
         )} <small>${cityTime.format("A")}</small></div>
-      </div>`;
+      </div>
+      `;
 }
 
 updateTime();
